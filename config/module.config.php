c31ce9b9cf64f56d64bd41b95bee6babf233ac8e
Marco Pivetta <ocramius@gmail.com>
2012-08-13 17:10:47 +0200

    Adding basic implementation of the resolvers (based on Zend\View's)

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,8 @@
 return array(
     'service_manager' => array (
         'factories' => array (
-            'asset_manager' => 'AssetManager\Service\AssetManagerServiceFactory'
+            'asset_manager'  => 'AssetManager\Service\AssetManagerServiceFactory',
+            'asset_resolver' => 'AssetManager\Service\ResolverServiceFactory'
         )
     ),
 
---
+++ src/AssetManager/Resolver/MapResolver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AssetManager\Resolver;
+
+use Traversable;
+use Zend\Stdlib\ArrayUtils;
+
+class MapResolver implements ResolverInterface
+{
+    /**
+     * @var array
+     */
+    protected $map = array();
+
+    /**
+     * Constructor
+     *
+     * Instantiate and optionally populate template map.
+     *
+     * @param  array|Traversable $map
+     */
+    public function __construct($map = array())
+    {
+        $this->setMap($map);
+    }
+
+    /**
+     * Set (overwrite) template map
+     *
+     * Maps should be arrays or Traversable objects with name => path pairs
+     *
+     * @param  array|Traversable $map
+     * @return self
+     */
+    public function setMap($map)
+    {
+        if (!is_array($map) && !$map instanceof Traversable) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s: expects an array or Traversable, received "%s"',
+                __METHOD__,
+                (is_object($map) ? get_class($map) : gettype($map))
+            ));
+        }
+
+        if ($map instanceof Traversable) {
+            $map = ArrayUtils::iteratorToArray($map);
+        }
+
+        $this->map = $map;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the template map
+     *
+     * @return array
+     */
+    public function getMap()
+    {
+        return $this->map;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function resolve($name)
+    {
+        return isset($this->map[$name]) ? $this->map[$name] : null;
+    }
+}
