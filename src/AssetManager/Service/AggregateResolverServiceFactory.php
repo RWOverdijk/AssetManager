dd2a0fb6e70e2c9b43449ac0e3dfb84987ceabc3
Roberto Wesley Overdijk <r.w.overdijk@gmail.com>
2012-08-14 14:01:26 +0200

    Cleanup old

--- src/AssetManager/Service/AggregateResolverServiceFactory.php
+++ src/AssetManager/Service/AggregateResolverServiceFactory.php
@@ -5,8 +5,6 @@ namespace AssetManager\Service;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use AssetManager\Resolver\AggregateResolver;
-use AssetManager\Resolver\PathStack;
-use AssetManager\Resolver\MapResolver;
 use AssetManager\Exception;
 
 /**
