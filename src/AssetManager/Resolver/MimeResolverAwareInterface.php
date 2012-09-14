bef16a0ae0b0ed340eba7e5a7d6c9fe6d2983340
Wesley Overdijk <r.w.overdijk@gmail.com>
2012-09-14 17:29:18 +0200

    Added missing methods

--- src/AssetManager/Resolver/MimeResolverAwareInterface.php
+++ src/AssetManager/Resolver/MimeResolverAwareInterface.php
@@ -2,8 +2,24 @@
 
 namespace AssetManager\Resolver;
 
+use AssetManager\Service\MimeResolver;
+
 /**
  * Interface to be implemented in resolver, whenever it requires the mime-resolver.
  */
 interface MimeResolverAwareInterface
-{}
+{
+    /**
+     * Set the MimeResolver.
+     *
+     * @param MimeResolver $resolver
+     */
+    public function setMimeResolver(MimeResolver $resolver);
+
+    /**
+     * Get the MimeResolver
+     *
+     * @return MimeResolver
+     */
+    public function getMimeResolver();
+}
