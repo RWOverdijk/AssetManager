f930c7f7369b29b1b25d29f6d3e85db3961ef124
Sebastian Kors <s.kors@unico-soft.com>
2013-11-19 19:06:16 +0100

    add tests for ConcatResolver
    RWOverdijk/AssetManager#110

---
+++ tests/AssetManagerTest/Resolver/ConcatResolverTest.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace AssetManagerTest\Service;
+
+use PHPUnit_Framework_TestCase;
+use Assetic\Asset;
+use AssetManager\Resolver\ConcatResolver;
+use AssetManager\Resolver\AggregateResolverAwareInterface;
+use AssetManager\Resolver\ResolverInterface;
+
+class ConcatIterable implements \IteratorAggregate
+{
+    public $mapName1 = array(
+        'map 1.1',
+        'map 1.2',
+        'map 1.3',
+        'map 1.4',
+    );
+
+    public $mapName2 = array(
+        'map 2.1',
+        'map 2.2',
+        'map 2.3',
+        'map 2.4',
+    );
+
+    public $mapName3 = array(
+        'map 3.1',
+        'map 3.2',
+        'map 3.3',
+        'map 3.4',
+    );
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this);
+    }
+}
+
+class ConcatResolverTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $resolver = new ConcatResolver(
+            array(
+                 'key1' => array(
+                     __FILE__
+                 ),
+                 'key2' => array(
+                     __FILE__
+                 ),
+            )
+        );
+
+        $this->assertTrue($resolver instanceof ResolverInterface);
+        $this->assertTrue($resolver instanceof AggregateResolverAwareInterface);
+
+        $this->assertSame(
+            array(
+                 'key1' => array(
+                     __FILE__
+                 ),
+                 'key2' => array(
+                     __FILE__
+                 ),
+            ),
+            $resolver->getConcats()
+        );
+    }
+
+    public function testSetGetAggregateResolver()
+    {
+        $resolver = new ConcatResolver;
+
+        $aggregateResolver = $this->getMock('AssetManager\Resolver\ResolverInterface');
+        $aggregateResolver
+            ->expects($this->once())
+            ->method('resolve')
+            ->with('say')
+            ->will($this->returnValue('world'));
+
+        $resolver->setAggregateResolver($aggregateResolver);
+
+        $this->assertEquals('world', $resolver->getAggregateResolver()->resolve('say'));
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testSetAggregateResolverFails()
+    {
+        $resolver = new ConcatResolver;
+
+        $resolver->setAggregateResolver(new \stdClass);
+    }
+
+    public function testSetConcatSuccess()
+    {
+        $resolver = new ConcatResolver;
+        $resolver->setConcats(new ConcatIterable);
+
+        $this->assertEquals(
+            array(
+                 'mapName1' => array(
+                     'map 1.1',
+                     'map 1.2',
+                     'map 1.3',
+                     'map 1.4',
+                 ),
+                 'mapName2' => array(
+                     'map 2.1',
+                     'map 2.2',
+                     'map 2.3',
+                     'map 2.4',
+                 ),
+                 'mapName3' => array(
+                     'map 3.1',
+                     'map 3.2',
+                     'map 3.3',
+                     'map 3.4',
+                 )
+            ), $resolver->getConcats()
+        );
+    }
+
+    /**
+     * @expectedException AssetManager\Exception\InvalidArgumentException
+     */
+    public function testSetConcatFails()
+    {
+        $resolver = new ConcatResolver;
+        $resolver->setConcats(new \stdClass);
+    }
+
+    public function testGetConcat()
+    {
+        $resolver = new ConcatResolver;
+        $this->assertSame(array(), $resolver->getConcats());
+    }
+
+    public function testResolveNull()
+    {
+        $resolver = new ConcatResolver;
+        $this->assertNull($resolver->resolve('bacon'));
+    }
+
+    public function testResolveAssetFail()
+    {
+        $resolver = new ConcatResolver;
+
+        $asset1 = array(
+            'bacon' => 'yummy',
+        );
+
+        $this->assertNull($resolver->setConcats($asset1));
+    }
+
+    public function testResolveAssetSuccess()
+    {
+        $resolver = new ConcatResolver;
+
+        $asset1 = array(
+            'bacon' => array(
+                __FILE__,
+                __FILE__,
+            ),
+        );
+
+        $callback = function ($file) {
+            $asset = new \Assetic\Asset\FileAsset(
+                $file
+            );
+
+            return $asset;
+        };
+
+        $aggregateResolver = $this->getMock('AssetManager\Resolver\ResolverInterface');
+        $aggregateResolver
+            ->expects($this->exactly(2))
+            ->method('resolve')
+            ->will($this->returnCallback($callback));
+        $resolver->setAggregateResolver($aggregateResolver);
+
+        $assetFilterManager = new \AssetManager\Service\AssetFilterManager();
+        $resolver->setAssetFilterManager($assetFilterManager);
+
+        $resolver->setConcats($asset1);
+
+        $asset      = $resolver->resolve('bacon');
+
+        $this->assertTrue($asset instanceof \AssetManager\Asset\ConcatStringAsset);
+        $this->assertEquals(
+            $asset->dump(),
+            file_get_contents(__FILE__)
+            ."\n"
+            .file_get_contents(__FILE__)
+        );
+    }
+}
