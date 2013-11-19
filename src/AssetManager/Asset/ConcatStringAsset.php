0b2dc06622623f8b990594b4bafd5e32b42f5af1
Sebastian Kors <s.kors@unico-soft.com>
2013-11-19 19:05:38 +0100

    add concat resolver for files with factory
    RWOverdijk/AssetManager#110

---
+++ src/AssetManager/Asset/ConcatStringAsset.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2013 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AssetManager\Asset;
+
+use Assetic\Filter\FilterInterface;
+use Assetic\Asset\BaseAsset;
+
+/**
+ * Represents a string asset.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
+class ConcatStringAsset extends BaseAsset
+{
+    private $lastModified;
+
+    /**
+     * Constructor.
+     *
+     * @param string $content    The content of the asset
+     * @param array  $filters    Filters for the asset
+     * @param string $sourceRoot The source asset root directory
+     * @param string $sourcePath The source asset path
+     */
+    public function __construct($content, $filters = array(), $sourceRoot = null, $sourcePath = null)
+    {
+        $this->setContent($content);
+
+        parent::__construct($filters, $sourceRoot, $sourcePath);
+    }
+
+    public function load(FilterInterface $additionalFilter = null)
+    {
+        $this->doLoad($this->getContent(), $additionalFilter);
+    }
+
+    public function setLastModified($lastModified)
+    {
+        $this->lastModified = $lastModified;
+    }
+
+    public function getLastModified()
+    {
+        return $this->lastModified;
+    }
+}
---
+++ src/AssetManager/Resolver/ConcatResolver.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace AssetManager\Resolver;
+
+use Traversable;
+use Zend\Stdlib\ArrayUtils;
+use Assetic\Asset\AssetInterface;
+use AssetManager\Asset\ConcatStringAsset;
+use AssetManager\Exception;
+use AssetManager\Service\AssetFilterManagerAwareInterface;
+use AssetManager\Service\AssetFilterManager;
+
+/**
+ * This resolver allows the resolving of concatenated files.
+ * Concatted files are added as an StringAsset and filters get applied to concatenated string.
+ */
+class ConcatResolver implements
+    ResolverInterface,
+    AggregateResolverAwareInterface,
+    AssetFilterManagerAwareInterface
+{
+    /**
+     * @var ResolverInterface
+     */
+    protected $aggregateResolver;
+
+    /**
+     * @var AssetFilterManager The filterManager service.
+     */
+    protected $filterManager;
+
+    /**
+     * @var array the concats
+     */
+    protected $concats = array();
+
+    /**
+     * Constructor
+     *
+     * Instantiate and optionally populate concats.
+     *
+     * @param array|Traversable $concats
+     */
+    public function __construct($concats = array())
+    {
+        $this->setConcats($concats);
+    }
+
+    /**
+     * Set (overwrite) concats
+     *
+     * Concats should be arrays or Traversable objects with name => path pairs
+     *
+     * @param  array|Traversable                  $concats
+     * @throws Exception\InvalidArgumentException
+     */
+    public function setConcats($concats)
+    {
+        if (!is_array($concats) && !$concats instanceof Traversable) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s: expects an array or Traversable, received "%s"',
+                __METHOD__,
+                (is_object($concats) ? get_class($concats) : gettype($concats))
+            ));
+        }
+
+        if ($concats instanceof Traversable) {
+            $concats = ArrayUtils::iteratorToArray($concats);
+        }
+
+        $this->concats = $concats;
+    }
+
+    /**
+     * Set the aggregate resolver.
+     *
+     * @param ResolverInterface $aggregateResolver
+     */
+    public function setAggregateResolver(ResolverInterface $aggregateResolver)
+    {
+        $this->aggregateResolver = $aggregateResolver;
+    }
+
+    /**
+     * Get the aggregate resolver.
+     *
+     * @return ResolverInterface
+     */
+    public function getAggregateResolver()
+    {
+        return $this->aggregateResolver;
+    }
+
+    /**
+     * Retrieve the concats
+     *
+     * @return array
+     */
+    public function getConcats()
+    {
+        return $this->concats;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function resolve($name)
+    {
+        if (!isset($this->concats[$name])) {
+            return null;
+        }
+
+        if (!is_array($this->concats[$name])) {
+            throw new Exception\RuntimeException(
+                "Concat with name $name is not an an array."
+            );
+        }
+
+        $mimeType   = null;
+        $content = "";
+        $lastModified = 0;
+
+        foreach ($this->concats[$name] as $asset) {
+
+            if (!is_string($asset)) {
+                throw new Exception\RuntimeException(
+                    'Asset should be of type string. got ' . gettype($asset)
+                );
+            }
+            if (null === ($res = $this->getAggregateResolver()->resolve($asset))) {
+                throw new Exception\RuntimeException("Asset '$asset' could not be found.");
+            }
+
+            if (!$res instanceof AssetInterface) {
+                throw new Exception\RuntimeException(
+                    "Asset '$asset' does not implement Assetic\\Asset\\AssetInterface."
+                );
+            }
+
+            //$this->getAssetFilterManager()->setFilters($asset, $res);
+
+            //$mimeType = $res->mimetype;
+            $content .= "\n".$res->dump();
+
+            if ($res->getLastModified() > $lastModified) {
+                $lastModified = $res->getLastModified();
+            }
+        }
+
+        $stringAsset = new ConcatStringAsset(ltrim($content));
+        $stringAsset->setLastModified($lastModified);
+        $stringAsset->mimetype = $mimeType;
+        $stringAsset->load();
+        //$this->getAssetFilterManager()->setFilters($name, $stringAsset);
+
+        return $stringAsset;
+    }
+
+    /**
+     * Set the AssetFilterManager.
+     *
+     * @param AssetFilterManager $filterManager
+     */
+    public function setAssetFilterManager(AssetFilterManager $filterManager)
+    {
+        $this->filterManager = $filterManager;
+    }
+
+    /**
+     * Get the AssetFilterManager
+     *
+     * @return AssetFilterManager
+     */
+    public function getAssetFilterManager()
+    {
+        return $this->filterManager;
+    }
+}
