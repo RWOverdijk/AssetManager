c0619125bb70e0c649da50ab21908a1fdf41328e
Marco Pivetta <ocramius@gmail.com>
2014-03-25 17:52:56 +0100

    Typo fix

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -40,7 +40,7 @@ if (file_exists(__DIR__ . '/TestConfiguration.php')) {
     $config = require __DIR__ . '/TestConfiguration.php';
 } else {
     $config = require __DIR__ . '/TestConfiguration.php.dist';
-}=
+}
 
 $serviceManager = new ServiceManager(new ServiceManagerConfig(
     isset($config['service_manager']) ? $config['service_manager'] : array()
