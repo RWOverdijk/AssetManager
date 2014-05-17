86fdd4ead5446852867fd0d0c2caf1b97b1e48ff
Sebastian Kors <s.kors@unico-soft.com>
2014-05-17 14:29:42 +0200

    code style: use separate file for class with test data

--- tests/AssetManagerTest/Resolver/ConcatResolverTest.php
+++ tests/AssetManagerTest/Resolver/ConcatResolverTest.php
@@ -9,35 +9,6 @@ use AssetManager\Resolver\AggregateResolverAwareInterface;
 use AssetManager\Resolver\ResolverInterface;
 use AssetManager\Service\MimeResolver;
 
-class ConcatIterable implements \IteratorAggregate
-{
-    public $mapName1 = array(
-        'map 1.1',
-        'map 1.2',
-        'map 1.3',
-        'map 1.4',
-    );
-
-    public $mapName2 = array(
-        'map 2.1',
-        'map 2.2',
-        'map 2.3',
-        'map 2.4',
-    );
-
-    public $mapName3 = array(
-        'map 3.1',
-        'map 3.2',
-        'map 3.3',
-        'map 3.4',
-    );
-
-    public function getIterator()
-    {
-        return new \ArrayIterator($this);
-    }
-}
-
 class ConcatResolverTest extends PHPUnit_Framework_TestCase
 {
     public function testConstruct()
---
+++ tests/AssetManagerTest/Service/ConcatIterable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AssetManagerTest\Service;
+
+class ConcatIterable implements \IteratorAggregate
+{
+    public $mapName1 = array(
+        'map 1.1',
+        'map 1.2',
+        'map 1.3',
+        'map 1.4',
+    );
+
+    public $mapName2 = array(
+        'map 2.1',
+        'map 2.2',
+        'map 2.3',
+        'map 2.4',
+    );
+
+    public $mapName3 = array(
+        'map 3.1',
+        'map 3.2',
+        'map 3.3',
+        'map 3.4',
+    );
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this);
+    }
+}
