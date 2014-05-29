0a71547faa6bfa2b7b64acb27f963da822e09e3c
Westin Shafer <wshafer@relivinc.com>
2014-05-29 09:52:54 -0500

    Added test to ensure that all defined services are retrievable.

---
+++ tests/AssetManagerTest/Config/ModuleServiceManagerConfigTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AssetManagerTest\Config;
+
+use PHPUnit_Framework_TestCase;
+use Zend\ServiceManager\Config;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Test to ensure config file is properly setup and all services are retrievable
+ *
+ * @package AssetManagerTest\Config
+ */
+class ModuleServiceManagerConfigTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the Service Managers Factories.
+     *
+     * @coversNothing
+     */
+    public function testServiceManagerFactories()
+    {
+        $config = include __DIR__.'/../../../config/module.config.php';
+
+        $serviceManagerConfig = new Config($config['service_manager']);
+        $serviceManager = new ServiceManager($serviceManagerConfig);
+        $serviceManager->setService('config', $config);
+
+        foreach ($config['service_manager']['factories'] as $serviceName => $service) {
+            $this->assertTrue($serviceManager->has($serviceName));
+
+            //Make sure we can fetch the service
+            $service = $serviceManager->get($serviceName);
+
+            $this->assertTrue(is_object($service));
+        }
+
+    }
+
+    /**
+     * Test the Service Managers Invokables.
+     *
+     * @coversNothing
+     */
+    public function testServiceManagerInvokables()
+    {
+        $config = include __DIR__.'/../../../config/module.config.php';
+
+        $serviceManagerConfig = new Config($config['service_manager']);
+        $serviceManager = new ServiceManager($serviceManagerConfig);
+        $serviceManager->setService('config', $config);
+
+        foreach ($config['service_manager']['invokables'] as $serviceName => $service) {
+            $this->assertTrue($serviceManager->has($serviceName));
+
+            //Make sure we can fetch the service
+            $service = $serviceManager->get($serviceName);
+
+            $this->assertTrue(is_object($service));
+        }
+    }
+
+    /**
+     * Test the Service Managers Invokables.
+     *
+     * @coversNothing
+     */
+    public function testServiceManagerAliases()
+    {
+        $config = include __DIR__.'/../../../config/module.config.php';
+
+        $serviceManagerConfig = new Config($config['service_manager']);
+        $serviceManager = new ServiceManager($serviceManagerConfig);
+        $serviceManager->setService('config', $config);
+
+        foreach ($config['service_manager']['aliases'] as $serviceName => $service) {
+            $this->assertTrue($serviceManager->has($serviceName));
+
+            //Make sure we can fetch the service
+            $service = $serviceManager->get($serviceName);
+
+            $this->assertTrue(is_object($service));
+        }
+    }
+
+}
