260fe704719c4968e8de9fb42d3b1a5581e0390e
Oscar <oscar.nesis@gmail.com>
2015-08-17 10:40:49 +0200

    Configurable query string param

--- src/AssetManager/View/Helper/Asset.php
+++ src/AssetManager/View/Helper/Asset.php
@@ -19,10 +19,15 @@ class Asset extends AbstractHelper
 
             $assetsPath = $this->config['asset_manager']['caching']['default']['options']['dir'];
 
+            // query string params
+            $queryString = isset($this->config['asset_manager']['query_string'])
+                ? $this->config['asset_manager']['query_string']
+                : '_';
+
             // find the file and if it exists, append its unix modification time to the filename
             $originalPath = $assetsPath . $filename;
             if (file_exists($originalPath)) {
-                return $filename . '?u=' . filemtime($originalPath);
+                return $filename . '?' . $queryString . '=' . filemtime($originalPath);
             }
         }
 
